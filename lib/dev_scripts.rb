fb47b015e92f62e9fdf1f19b4107850b5a2f0bc7
Carlos Quinones <cquinones100@gmail.com>
2019-11-30 14:28:52 -0500

    initial commit

---
+++ lib/dev_scripts.rb
@@ -0,0 +1,5 @@
+require "dev_scripts/version"
+
+module DevScripts
+  class Error < StandardError; end
+end
---
+++ spec/dev_scripts_spec.rb
@@ -0,0 +1,5 @@
+RSpec.describe DevScripts do
+  it "has a version number" do
+    expect(DevScripts::VERSION).not_to be nil
+  end
+end
