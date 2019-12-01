5fbfb1b5b427d39f0f9659247883ea93e119f971
Carlos Quinones <cquinones100@gmail.com>
2019-12-01 14:46:12 -0500

    handle a custom script directory

--- lib/dev_scripts.rb
+++ lib/dev_scripts.rb
@@ -7,4 +7,10 @@ end
 require 'dev_scripts/scripts/open_spec_file'
 require 'dev_scripts/scripts/rubocop_metrics_method_length'
 
+if File.exist? './dev_scripts'
+  Dir['./dev_scripts/**/*.rb'].each do |file|
+    require file
+  end
+end
+
 DevScripts::Script.execute(ARGV)
