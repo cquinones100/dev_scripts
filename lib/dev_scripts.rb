18c1683dea0a98d06296754d334eb7eadb5bc05f
Carlos Quinones <cquinones100@gmail.com>
2019-12-04 22:22:22 -0500

    require expand_method_args

--- lib/dev_scripts.rb
+++ lib/dev_scripts.rb
@@ -7,6 +7,7 @@ end
 require 'dev_scripts/scripts/open_spec_file'
 require 'dev_scripts/scripts/rubocop_metrics_method_length'
 require 'dev_scripts/scripts/expand_block'
+require 'dev_scripts/scripts/expand_method_args'
 
 if File.exist? './dev_scripts'
   Dir['./dev_scripts/**/*.rb'].each do |file|
